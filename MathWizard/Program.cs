0d1e381a6eabb538002676a5c3ec2fe27fed7b6f
Karan Nandwani <karann@microsoft.com>
2019-03-15 23:06:55 -0700

    Add project files.

---
+++ MathWizard/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathWizard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
